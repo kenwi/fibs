88bea8f719e67807d3840c1b610b6099de974a2c
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-05-14 12:47:32 +0200

    Using Binet's formula, and SIMD

---
+++ Binet-SIMD.cs
@@ -0,0 +1,40 @@
+﻿namespace BinetsFormula
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Numerics;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double phi = (1 + Math.Sqrt(5)) / 2;
+            double computeNth(double n) => Math.Floor(Math.Pow(phi, n) / Math.Sqrt(5) + 0.5);
+            Vector<double> compute4n(double n) => new Vector<double>(new double[] {
+                computeNth(n),
+                computeNth(n + 1),
+                computeNth(n + 2),
+                computeNth(n + 3)
+            });
+                        
+            int i = 0, numFibs = 10, vecSize = Vector<double>.Count;
+            double [] result = new double[numFibs];
+            
+            for (; i < numFibs - vecSize + 1; i += vecSize)
+            {
+                var v = compute4n(i);
+                v.CopyTo(result, i);
+            }
+            if (i != numFibs)
+            {
+                for (; i < numFibs; i++)
+                {
+                    var v = computeNth(i);
+                    result[i] = v;
+                }
+            }
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
