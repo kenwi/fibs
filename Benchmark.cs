a1b2983e59bc93a1b6a59568edc2fd98fde885c4
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-05-17 16:50:12 +0200

    Update benchmark order

--- Benchmark.cs
+++ Benchmark.cs
@@ -1,5 +1,4 @@
-﻿
-namespace fibs
+﻿namespace fibs
 {
     using System;
     using System.Diagnostics;
@@ -18,89 +17,25 @@ namespace fibs
         readonly float phi_f = (1 + MathF.Sqrt(5)) * 0.5f;
         protected int index, vecSize, numFibs = 1000;
         
-        float[] floatNumbers;
-        double[] doubleNumbers;
+        float[] floats;
+        double[] doubles;
         
         BenchmarkComputation<float> genericFloatComputation;
         BenchmarkComputation<double> genericDoubleComputation;
 
         public Benchmark()
         {
-            doubleNumbers = new double[numFibs];
+            doubles = new double[numFibs];
             genericDoubleComputation = new BenchmarkComputation<double>();
             genericDoubleComputation.ComputeMultiple = (numbers, index) => Compute4n_d(index).CopyTo(numbers, index);
             genericDoubleComputation.ComputeSingle = (numbers, index) => numbers[index] = ComputeN_dexp(index);
 
-            floatNumbers = new float[numFibs];
+            floats = new float[numFibs];
             genericFloatComputation = new BenchmarkComputation<float>();
             genericFloatComputation.ComputeMultiple = (numbers, index) => Compute8n_f(index).CopyTo(numbers, index);
             genericFloatComputation.ComputeSingle = (numbers, index) => numbers[index] = ComputeN_fexp(index);
         }
         
-        [Benchmark]
-        public double[] RunDoubles()
-        {
-            vecSize = Vector<double>.Count;
-            for (; index < numFibs - vecSize + 1; index += vecSize)
-            {
-                Compute4n_d(index).CopyTo(doubleNumbers, index);
-            }
-            while (index != numFibs)
-            {
-                doubleNumbers[index++] = ComputeN_dexp(index);
-            }
-            index = 0;
-            return doubleNumbers;
-        }
-
-        [Benchmark]
-        public float[] RunFloats()
-        {
-            vecSize = Vector<float>.Count;
-            for (; index < numFibs - vecSize + 1; index += vecSize)
-            {
-                Compute8n_f(index).CopyTo(floatNumbers, index);
-            }
-            while (index != numFibs)
-            {
-                floatNumbers[index++] = ComputeN_fexp(index);
-            }
-            index = 0;
-            return floatNumbers;
-        }
-
-        [Benchmark]
-        public double[] RunGenericDoubles()
-        {
-            vecSize = Vector<double>.Count;
-            for (; index < numFibs - vecSize + 1; index += vecSize)
-            {
-                genericDoubleComputation.ComputeMultiple(doubleNumbers, index);
-            }
-            while (index != numFibs)
-            {
-                genericDoubleComputation.ComputeSingle(doubleNumbers, index++);
-            }
-            index = 0;
-            return doubleNumbers;
-        }
-
-        [Benchmark]
-        public float[] RunGenericFloats()
-        {
-            vecSize = Vector<float>.Count;
-            for (; index < numFibs - vecSize + 1; index += vecSize)
-            {
-                genericFloatComputation.ComputeMultiple(floatNumbers, index);
-            }
-            while (index != numFibs)
-            {
-                genericFloatComputation.ComputeSingle(floatNumbers, index++);
-            }
-            index = 0;
-            return floatNumbers;
-        }
-
         [Benchmark]
         [Arguments(10)]
         public double ComputeN_d(double n) => Math.Floor(Math.Pow(phi_d, n) / Math.Sqrt(5) + 0.5);
@@ -138,5 +73,69 @@ namespace fibs
             ComputeN_fexp(n + 6),
             ComputeN_fexp(n + 7)
         });
+
+        [Benchmark]
+        public double[] RunDoubles()
+        {
+            vecSize = Vector<double>.Count;
+            for (; index < numFibs - vecSize + 1; index += vecSize)
+            {
+                Compute4n_d(index).CopyTo(doubles, index);
+            }
+            while (index != numFibs)
+            {
+                doubles[index++] = ComputeN_dexp(index);
+            }
+            index = 0;
+            return doubles;
+        }
+
+        [Benchmark]
+        public float[] RunFloats()
+        {
+            vecSize = Vector<float>.Count;
+            for (; index < numFibs - vecSize + 1; index += vecSize)
+            {
+                Compute8n_f(index).CopyTo(floats, index);
+            }
+            while (index != numFibs)
+            {
+                floats[index++] = ComputeN_fexp(index);
+            }
+            index = 0;
+            return floats;
+        }
+
+        [Benchmark]
+        public double[] RunGenericDoubles()
+        {
+            vecSize = Vector<double>.Count;
+            for (; index < numFibs - vecSize + 1; index += vecSize)
+            {
+                genericDoubleComputation.ComputeMultiple(doubles, index);
+            }
+            while (index != numFibs)
+            {
+                genericDoubleComputation.ComputeSingle(doubles, index++);
+            }
+            index = 0;
+            return doubles;
+        }
+
+        [Benchmark]
+        public float[] RunGenericFloats()
+        {
+            vecSize = Vector<float>.Count;
+            for (; index < numFibs - vecSize + 1; index += vecSize)
+            {
+                genericFloatComputation.ComputeMultiple(floats, index);
+            }
+            while (index != numFibs)
+            {
+                genericFloatComputation.ComputeSingle(floats, index++);
+            }
+            index = 0;
+            return floats;
+        }
     }
 }
--- README.md
+++ README.md
@@ -8,13 +8,13 @@ Intel Core i7-6700K CPU 4.00GHz (Skylake), 1 CPU, 8 logical and 4 physical cores
 
 |            Method |  n |         Mean |       Error |        StdDev |
 |------------------ |--- |-------------:|------------:|--------------:|
-|        RunDoubles |  ? | 20,228.82 ns | 475.3984 ns |   711.5539 ns |
-|         RunFloats |  ? | 48,781.02 ns | 952.1425 ns | 1,096.4888 ns |
-| RunGenericDoubles |  ? | 22,229.58 ns | 192.6551 ns |   160.8757 ns |
-|  RunGenericFloats |  ? | 49,733.11 ns | 966.6905 ns |   992.7200 ns |
 |        ComputeN_d | 10 |     27.57 ns |   0.6113 ns |     0.5419 ns |
 |        ComputeN_f | 10 |     11.28 ns |   0.2628 ns |     0.2459 ns |
 |     ComputeN_fexp | 10 |     14.41 ns |   0.3311 ns |     0.3400 ns |
 |     ComputeN_dexp | 10 |     17.67 ns |   0.3013 ns |     0.2818 ns |
 |       Compute4n_d | 10 |     76.58 ns |   1.4503 ns |     1.3566 ns |
 |       Compute8n_f | 10 |    117.08 ns |   2.3343 ns |     2.4977 ns |
+|        RunDoubles |  ? | 20,228.82 ns | 475.3984 ns |   711.5539 ns |
+|         RunFloats |  ? | 48,781.02 ns | 952.1425 ns | 1,096.4888 ns |
+| RunGenericDoubles |  ? | 22,229.58 ns | 192.6551 ns |   160.8757 ns |
+|  RunGenericFloats |  ? | 49,733.11 ns | 966.6905 ns |   992.7200 ns |
